2da71bf915c0702f2932a5bfde658bad9b3a93c1
mitz <mitsuru.sakai@gmail.com>
2019-11-23 11:08:11 +0900

    first commit of app_openGrep

---
+++ app_openGrep/config/setting.js
@@ -0,0 +1,16 @@
+var CONFIG = {
+  "DEBUG_MODE" : false,
+  "APP_NAME" : "app_openGrep.jse",
+  "DIRS" : {
+    "MY_FOLDER" : "G:\\Users\\sakai\\OneDrive\\Documents",
+    "SAKURA_MACRO" : "G:\\Users\\sakai\\AppData\\Roaming\\sakura",
+    "JOURNAL" : "G:\\Users\\sakai\\OneDrive\\Documents\\journal",
+    "PWSH_SCRIPT" : "G:\Users\\sakai\\OneDrive\\Documents\\WindowsPowerShell"
+  },
+  "EXTS" :  {
+    "MY_FOLDER" : '*.md;*.txt;',
+    "SAKURA_MACRO" : '*.jse;*.js;#.git;#sandbox',
+    "JOURNAL" : '*.md',
+    "PWSH_SCRIPT" : '*.ps1;*.psd1;#.git;#backup19'
+  }
+}
