a66302502b5330456a3c0d17bb51501ea39879a4
mitz <mitsuru.sakai@gmail.com>
2019-11-24 21:54:10 +0900

    add XMLFix

--- app_beautify/config/setting.js
+++ app_beautify/config/setting.js
@@ -2,6 +2,7 @@ var CONFIG = {
   "DEBUG_MODE": false,
   "APP_NAME": "app_beautify.jse",
   "EXE_SQLFIX": "G:\\Users\\sakai\\Downloads\\SQLFix\\SQLFix.exe",
+  "EXE_XMLFIX": "G:\\Users\\sakai\\Downloads\\XMLFix100\\XMLFix.exe",
   "EXE_SAKURA_MACRO": "sakura.exe -DEBUGMODE -M=",
   "MACRO_FILE": 'paste_clipboard.jse',
   "BEAUTIFY_OPT": {
