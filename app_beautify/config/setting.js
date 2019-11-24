0629c0bf4be280b30683ceb81bde63995106a783
mitz <mitsuru.sakai@gmail.com>
2019-11-24 18:45:42 +0900

    complete for js-beautify

---
+++ app_beautify/config/setting.js
@@ -0,0 +1,7 @@
+var CONFIG = {
+  "DEBUG_MODE" : true,
+  "APP_NAME" : "app_beautify.jse",
+  "EXE_SQLFIX" : "G:\\Users\\sakai\\Downloads\\SQLFix\\SQLFix.exe",
+  "EXE_SAKURA_MACRO" : "sakura.exe -DEBUGMODE -M=",
+  "MACRO_FILE" : 'paste_clipboard.jse'
+  }
