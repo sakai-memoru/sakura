0b2b5cb2fffe65d58a2aa10456d86ea7113d16eb
mitz <mitsuru.sakai@gmail.com>
2019-12-02 21:58:07 +0900

    sync

--- config/setting.js
+++ config/setting.js
@@ -1,5 +1,5 @@
 var CONFIG = {
   "DEBUG_MODE" : true,
   "APP_NAME" : "base.js",
-  "TEST_TARGET_MACRO" : "G:\\Users\\sakai\\AppData\\Roaming\\sakura\\sandbox\\apps\\app_postMdParagraph.jse"
+  "TEST_TARGET_MACRO" : "G:\\Users\\sakai\\AppData\\Roaming\\sakura\\sandbox\\apps\\app_commentInOut.jse"
 }
