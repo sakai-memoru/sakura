54c24ecef8139b93952e01b8161310035b78b4a7
mitz <mitsuru.sakai@gmail.com>
2019-12-02 21:56:55 +0900

    add app_searchSnippets.jse

---
+++ app_searchSnippets/config/setting.js
@@ -0,0 +1,7 @@
+// NOTICE:JSON記述が正しくないとevalでエラーとなり、CONFIGを読み込めない
+var CONFIG = {
+  "DEBUG_MODE": false,
+  "APP_NAME": "app_searchSnippets.jse",
+  "SNIPPETS_FOLDER": "G:\\Users\\sakai\\AppData\\Roaming\\sakura\\snippet",
+  "ENCODE" : "utf-8"
+}
