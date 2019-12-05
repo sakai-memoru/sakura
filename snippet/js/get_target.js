a561d739f02f1296c663dad688c2a19b8ccf6a4b
mitz <mitsuru.sakai@gmail.com>
2019-12-06 08:20:35 +0900

    add $L selected

--- snippet/js/get_target.js
+++ snippet/js/get_target.js
@@ -5,8 +5,10 @@ var get_target = function(expandParam){
   // $b : opened file's extention
   // $C : 選択中の場合、選択テキストの１行目のテキスト（改行コード除く）
   //      選択中でない場合、カーソル位置の単語
-  // 引数無し : 選択されている場合、選択された文字列。選択されていない場合、Clipboardにある文字列
+  // 引数無し : 選択されている場合、選択された文字列。
+  //       選択されていない場合、Clipboardにある文字列
   // $Z : 独自expandPrameter : 全テキスト
+  // $L : 独自expandPrameter : 行選択として取得
   var ret ='';
   if(typeof(expandParam) === 'undefined'){
     if(Editor.IsTextSelected){
@@ -20,10 +22,18 @@ var get_target = function(expandParam){
       ret = Editor.GetSelectedString(0);
       Editor.IsTextSelectingLock(0);
       Editor.ExpandParameter(expandParam)
+    } else if (expandParam === '$L'){
+      if(Editor.IsTextSelected){
+        // line select
+        var lineNum = get_seletedLineNumber();
+        ret = get_lines(lineNum.start_line_num, lineNum.end_line_num);
+        console.dir(lineNum);
+        console.log('L186 :' + ret);
+        select_lines(lineNum.start_line_num, lineNum.end_line_num);
+      }
     } else {
       ret = Editor.ExpandParameter(expandParam)
     }
   }
   return ret;
 };
-
