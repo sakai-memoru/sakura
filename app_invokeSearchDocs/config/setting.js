6afdf09513674e70865c9e844b6d6f2ced7ea915
mitz <mitsuru.sakai@gmail.com>
2019-12-06 08:19:30 +0900

    add app invoke serach docs

---
+++ app_invokeSearchDocs/config/setting.js
@@ -0,0 +1,15 @@
+// NOTICE:JSON記述が正しくないとevalでエラーとなり、CONFIGを読み込めない
+var get_documentFolder = function(){
+  var objShell = new ActiveXObject("WScript.Shell");
+  var doc_folder = objShell.SpecialFolders("MyDocuments");
+  objShell = null;
+  return doc_folder;
+}
+
+var CONFIG = {
+  "DEBUG_MODE": true,
+  "APP_NAME": "app_invokeSearchDocs.jse",
+  "TARGET_FOLDER": get_documentFolder(),
+  "TARGET_EXT": ['pptx', 'xlsx', 'md', 'pdf', 'asta'],
+  "ENCODE" : "utf-8"
+}
