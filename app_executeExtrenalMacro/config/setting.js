f177324be5af599103ab9a0bbc2fc1b9e3ec965a
mitz <mitsuru.sakai@gmail.com>
2019-12-06 05:51:40 +0900

    add execute external Macro

---
+++ app_executeExtrenalMacro/config/setting.js
@@ -0,0 +1,7 @@
+// NOTICE:JSON記述が正しくないとevalでエラーとなり、CONFIGを読み込めない
+var CONFIG = {
+  "DEBUG_MODE": true,
+  "APP_NAME": "app_executeExtrenalMacro.jse",
+  "MACRO_FOLDER": "",
+  "ENCODE" : "utf-8"
+}
