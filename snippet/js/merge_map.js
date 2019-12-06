06ca2a67abd7254ad1b39aedbe8b7bf24c257115
mitz <mitsuru.sakai@gmail.com>
2019-12-06 09:10:06 +0900

    modified some snippet

--- snippet/js/merge_map.js
+++ snippet/js/merge_map.js
@@ -1,35 +1,11 @@
-function replace_all(string, target, replacement) {
-  var result = "";
-  var offset = 0;
-  var target_length = target.length;
-  if (target_length === 0) {
-    for (var i = 0, c = string.length; i < c; i++) {
-      result += string[i];
-      result += replacement;
-    }
-    if (result.length)
-      return result.substr(0, result.length - replacement.length);
-    return result;
-  }
-  do {
-    var i = string.indexOf(target, offset);
-    if (i === -1) {
-      result += string.substring(offset);
-      return result;
-    }
-    result += string.substring(offset, i);
-    result += replacement;
-    offset = i + target_length;
-  } while (true);
-}
-
-function merge_map(template, map_){
-  for(key_ in map_){
+var merge_map = function(template, map_){
+  for (var key_ in map_){
     if(map_.hasOwnProperty(key_)){
-      var value_ = map_[key_];
-      template = replace_all(template, '{{' + key_ + '}}', value_);
+      // console.log("L148 : key_ = " + key_);
+      // console.log("L149 : map_[key_] = " + map_[key_]);
+      var regx = new RegExp('{{' + key_ + '}}',"gim");
+      template = template.replace(regx, map_[key_]);
     }
-  };
+  }
   return template;
 }
-
