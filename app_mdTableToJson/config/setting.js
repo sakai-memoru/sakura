f33897c28d13feb47afe65a487e591b17278ad10
mitz <mitsuru.sakai@gmail.com>
2019-12-14 06:41:47 +0900

    md to json

---
+++ app_mdTableToJson/config/setting.js
@@ -0,0 +1,14 @@
+// NOTICE:JSON記述が正しくないとevalでエラーとなり、CONFIGを読み込めない
+var CONFIG = {
+  "DEBUG_MODE": false,
+  "APP_NAME": "app_mdTableToJson.jse",
+  "ENCODE" : "utf-8"
+}
+
+CONFIG.IS_CHECKED_FORM = true;
+CONFIG.FORMAT_FNAME = 'FORMAT';
+CONFIG.FORMAT_SHEETID = 'TD';
+CONFIG.TABLE_HEAD = 'no';
+CONFIG.TABLE_HEAD_POS = 1;
+CONFIG.TABLE_COLSIZE = 7;
+
