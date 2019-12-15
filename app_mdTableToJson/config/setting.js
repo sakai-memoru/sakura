59cd8ffdc010ae1316495182fc6e558adb0679f6
mitz <mitsuru.sakai@gmail.com>
2019-12-15 21:03:14 +0900

    add md to json

--- app_mdTableToJson/config/setting.js
+++ app_mdTableToJson/config/setting.js
@@ -1,14 +1,15 @@
 // NOTICE:JSON記述が正しくないとevalでエラーとなり、CONFIGを読み込めない
 var CONFIG = {
-  "DEBUG_MODE": false,
+  "DEBUG_MODE": true,
   "APP_NAME": "app_mdTableToJson.jse",
   "ENCODE" : "utf-8"
 }
 
-CONFIG.IS_CHECKED_FORM = true;
+CONFIG.IS_CHECKED_FORM = false;
 CONFIG.FORMAT_FNAME = 'FORMAT';
 CONFIG.FORMAT_SHEETID = 'TD';
 CONFIG.TABLE_HEAD = 'no';
 CONFIG.TABLE_HEAD_POS = 1;
 CONFIG.TABLE_COLSIZE = 7;
-
+CONFIG.MACRO_FOLDER = "sakura";
+CONFIG.MACRO_FILE = "paste_clipboard.jse";
